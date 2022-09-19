e699816307d0f68b8cb2f9871ef5f882b89de589
dewebkiller <dewebkiller@gmail.com>
2022-09-19 14:36:09 +0545

    adding shortcode functions

--- shortcode.php
+++ shortcode.php
@@ -1,1 +1,12 @@
-gh repo clone dewebkiller/wp_shortcode
+<?php
+add_shortcode( 'lc_testimonial', 'lc_testimonial_shortcode' );
+function lc_testimonial_shortcode(){
+   ob_start(); ?>
+
+  <!-- HTML code here -->
+   
+   <?php
+   $output = ob_get_contents();  
+   ob_get_clean();
+   return $output;
+}
